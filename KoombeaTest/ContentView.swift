d48d9e198907e6410d1f7709aed04e42e54e04fb
ivancity <shieldm@hotmail.com>
2021-05-19 20:47:06 -0500

    Initial Commit

---
+++ KoombeaTest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  KoombeaTest
+//
+//  Created by Ivan on 5/19/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ KoombeaTest/KoombeaTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  KoombeaTestApp.swift
+//  KoombeaTest
+//
+//  Created by Ivan on 5/19/21.
+//
+
+import SwiftUI
+
+@main
+struct KoombeaTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
